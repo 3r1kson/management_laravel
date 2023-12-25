17e7296a23100213c07ad80ac6491bb6311c79d7
Erikson <eriksonribeiro84@gmail.com>
2023-12-25 20:39:10 -0300

    excluding reduntant pages

--- resources/views/app/produto/edit.blade.php
+++ resources/views/app/produto/edit.blade.php
@@ -15,27 +15,11 @@
     </div>
     <div class="informacao-pagina">
         {{ $msg ?? ''}}
-        <div style="width:30%; margin-left: auto; margin-right: auto;">
-            <form method="post" action="{{ route('produto.update', ['produto' => $produto->id]) }}">
-                {{-- <input type="hidden" name="id" value="{{ $produto->id ?? ''}}"> --}}
-                @csrf
-                @method('PUT')
-                <input type="text" value="{{ $produto->nome ?? old('nome')}}" name="nome" placeholder="Nome" class="borda-preta">
-                {{ $errors->has('nome') ? $errors->first('nome') : ''}}
-                <input type="text" value="{{ $produto->descricao ?? old('descricao')}}" name="descricao" placeholder="Descricao" class="borda-preta">
-                {{ $errors->has('descricao') ? $errors->first('descricao') : ''}}
-                <input type="text" value="{{ $produto->peso ?? old('peso')}}" name="peso" placeholder="Peso" class="borda-preta">
-                {{ $errors->has('peso') ? $errors->first('peso') : ''}}
-                <select name="unidade_id">
-                    <option>-- Selecione a unidade de medida</option>
-                    @foreach ($unidades as $unidade)
-                    <option value="{{ $unidade->id }}" {{ ( $produto->unidade_id ?? old('unidade_id')) == $unidade->id  ? 'selected' : ' '}}>{{ $unidade->descricao }}</option>
-                    @endforeach
-                    
-                </select>
-                {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : ''}}
-                <button type="submit" class="borda-preta">Cadastrar</button>
-            </form>
+        <div style="width:100%; margin-left: auto; margin-right: auto;">
+            <div style="width:30%; margin-left: auto; margin-right: auto;">
+                @component('app.produto._components.form_create_edit', ['produto' => $produto,'unidades' => $unidades])
+                @endcomponent
+            </div>
         </div>
     </div>
 </div>
